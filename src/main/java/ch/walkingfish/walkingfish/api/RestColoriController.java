b8cc210d2967c104cd4a997e6c7f007898abcd46
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-03-05 17:36:52 +0100

    Adding DELETE method in colori rest controller (#11)

--- src/main/java/ch/walkingfish/walkingfish/api/RestColoriController.java
+++ src/main/java/ch/walkingfish/walkingfish/api/RestColoriController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,5 +66,11 @@ public class RestColoriController {
         }
 
         return coloriService.updateColori(colori);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteColori(@PathVariable int id)
+    {
+        coloriService.deleteColori(id);
     }
 }
