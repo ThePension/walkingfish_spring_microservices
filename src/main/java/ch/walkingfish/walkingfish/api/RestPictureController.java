e0f0d1f17caf8d04e7b2306ef393a69ac7c3dc1e
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-03-06 09:34:19 +0100

    Adding DELETE method for image's pictures (#18)

--- src/main/java/ch/walkingfish/walkingfish/api/RestPictureController.java
+++ src/main/java/ch/walkingfish/walkingfish/api/RestPictureController.java
@@ -1,8 +1,10 @@
 package ch.walkingfish.walkingfish.api;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ch.walkingfish.walkingfish.model.Article;
 import ch.walkingfish.walkingfish.model.Picture;
+import ch.walkingfish.walkingfish.service.FileStorageService;
 import ch.walkingfish.walkingfish.service.PictureService;
 
 @RestController
 @RequestMapping("/api/picture")
 public class RestPictureController {
     @Autowired
-    PictureService pictureService;
+    private PictureService pictureService;
+
+    @Autowired
+    private FileStorageService fileStorageService;
 
     @GetMapping(value = {"/", ""})
     public List<Picture> getAllPictures()
@@ -42,6 +48,35 @@ public class RestPictureController {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePictureById(@PathVariable int id)
+    {
+        Picture picture;
+        // Get the picture from the database
+        try {
+            picture = pictureService.getPictureById((long) id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Delete the picture from the database
+        try {
+            pictureService.deletePictureInDB((long) id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Delete the picture from the server
+        try {
+            fileStorageService.delete(picture.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
         }
     }
 }
