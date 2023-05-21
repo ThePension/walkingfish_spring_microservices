4f23c411a5ecee1e9d4a0377828c155ef4f28d1b
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-05-21 18:14:41 +0200

    Log auth attempts

--- src/main/java/ch/walkingfish/walkingfish/api/AuthController.java
+++ src/main/java/ch/walkingfish/walkingfish/api/AuthController.java
@@ -13,20 +13,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.walkingfish.walkingfish.model.log.LogType;
+import ch.walkingfish.walkingfish.model.log.SimpleLog;
 import ch.walkingfish.walkingfish.security.jwt.JwtUtils;
+import ch.walkingfish.walkingfish.service.ProducerService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController { 
   @Autowired
-  AuthenticationManager authenticationManager;
+  private AuthenticationManager authenticationManager;
      
   @Autowired
-  PasswordEncoder encoder;
+  private PasswordEncoder encoder;
 
   @Autowired
-  JwtUtils jwtUtils;
+  private JwtUtils jwtUtils;
+
+  @Autowired
+  private ProducerService producerService;
 
   @PostMapping("/login")
   public ResponseEntity<?> authenticateUser(String username, String password) {
@@ -35,6 +41,11 @@ public class AuthController {
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
     String jwt = jwtUtils.generateTokenFromUsername(username);
+
+    // Log the login event
+    SimpleLog log = new SimpleLog(LogType.INFO, "POST /api/auth/login", "User " + username + " logged in");
+
+    producerService.send(log);
 
     // TODO : Generate better response, with more details
     return ResponseEntity.ok(jwt);
